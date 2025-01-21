7002eb59907e803cb7413a8f6a654c60bfb42dfe
AlanMartins00 <alanmartinsferreira59@gmail.com>
2025-01-21 17:29:39 -0300

    update

--- Create_User_Front-End/src/services/api.js
+++ Create_User_Front-End/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://create-users-uugl-git-main-alan-martins-projects-6e894c4d.vercel.app/"
+    baseURL: "https://create-users-uugl-git-main-alan-martins-projects-6e894c4d.vercel.app"
 })
 
 
